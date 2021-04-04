63eb6b070a580d9cfa2b0f1c96b13d5e9243ce8b
Keeven Oliveira <66172398+KeevenOliveira@users.noreply.github.com>
2021-04-04 02:02:15 -0300

    consuming API with JS

---
+++ scripts/index.js
@@ -0,0 +1,42 @@
+const card = document.getElementById("sec-container2");
+
+async function getContent(){
+    try {
+        const response = await fetch("https://frontend-intern-challenge-api.iurykrieger.now.sh/products")
+        const data = await response.json();
+
+        // console.log(response)
+        show(data)
+        
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getContent()
+
+function show({products}){
+
+    let output = '';
+
+    for (const product of products) {
+        
+        output +=`<div class="card">
+        <img class="img" src=${product.image}>`
+        output +=`<div class="card-txt">
+        <p class="card-p1">${product.name}</p>
+        <p class="card-p2">${product.description}</p>
+        <p class="card-p2">De: R$${product.oldPrice}</p>
+        <h3>Por: ${product.price}</h3>
+        <p class="card-p2">ou ${product.installments.count}x de R$${product.installments.value}</p>
+        </div>
+        <button class="btns-card">Comprar</button>
+        </div>`
+    }
+
+    card.innerHTML = output;
+}
+function reload(){
+    sessionStorage.setItem("Recarregou", "true");
+    window.location.reload();
+}
